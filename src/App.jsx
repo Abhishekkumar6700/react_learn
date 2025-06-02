4df29339dea3f81e49fa89adf3f28fb55c2f95f5
Abhishekkumar6700 <singhabhisingh8210@gmail.com>
2025-06-03 00:05:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+      <ul>
+        <li>apple</li>
+        <li>lichi</li>
+        <li>papaya</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
